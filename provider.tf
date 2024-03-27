8d871286ff445895862073a376df45650f1b364e
siddhesh261 <siddheshghode99@gmail.com>
2024-03-28 00:38:48 +0530

    2

--- provider.tf
+++ provider.tf
@@ -16,5 +16,4 @@ terraform {
 
 provider "aws"{
     region ="us-east-1"
-
 }
--- resources.tf
+++ resources.tf
@@ -1,6 +1,6 @@
 resource "aws_security_group" "webserver" {
     name = "Webserver1"
-    description = "this sg is meant for all the lab servers"
+    description = "this SG is meant for all the lab servers"
     vpc_id = data.aws_vpc.default.id
   
 }
